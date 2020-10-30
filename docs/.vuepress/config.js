05964cc2628b5fe648c5e671a861dd6aebc44485
ym6 <yangml.work@gmail.com>
2020-10-30 18:50:37 +0800

    update.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'OutReader',
   description: '工程师数据一站式解决方案',
-  base: '/',
+  base: '/outreaderjs/',
   themeConfig: {
     nav: [
       { text: '软件下载', link: '/download/' },
